58d7739e20b67c1d9410cae4320c4e8e539b2322
Pamela <pafreema@ualberta.ca>
2016-01-27 13:57:13 -0800

    more m83 plots

--- m83plots2.py
+++ m83plots2.py
@@ -9,19 +9,18 @@ import matplotlib.pyplot as plt
 from galaxies import Galaxy
 import astropy.units as u
 
-mygalaxy=Galaxy("m83")
+mygalaxy=Galaxy("M83")
 print(mygalaxy)
-#load the file
+
 mytable=Table.read('/home/pafreema/Documents/m83.co10_props_cprops.fits')
 
 idx = np.where(np.isfinite(mytable['VIRMASS_EXTRAP_DECONV'])) #removes values that are infinite/NaN
 
-rgal=mygalaxy.radius(ra=mytable['XPOS'], dec=mytable['YPOS'])
-#print rgal.to(u.pc)
+rgal=mygalaxy.radius(ra=(mytable['XPOS'][idx]), dec=(mytable['YPOS'][idx]))
+print rgal.to(u.pc) #get radii in parsecs
 
 sigma=(mytable['VRMS_EXTRAP_DECONV'][idx])/np.sqrt((mytable['RADRMS_EXTRAP_DECONV'][idx])**2) #sigma naught
 dens=(mytable['MASS_EXTRAP'][idx])/(np.pi*((mytable['RADRMS_EXTRAP_DECONV'][idx])**2)) #mass density
-rgal=np.sqrt((mytable['YRMS_EXTRAP_DECONV'][idx])**2+(mytable['XRMS_EXTRAP_DECONV'][idx])**2) #galactocentric radius?
 
 m,b=np.polyfit(dens,sigma,1)
 x=np.linspace(0,20000,100)
@@ -29,12 +28,12 @@ figure=plt.figure(figsize=(4.5,4))
 plt.plot (dens, sigma, marker='s', linestyle='None')
 plt.plot(x, m*x+b)
 plt.xlabel(r'${\Sigma}(M)\ (M_{\odot}/pc^2)$')
-plt.ylabel(r'${\sigma}_0$')
+plt.ylabel(r'${\sigma}_0\ $')
 plt.tight_layout()
 plt.savefig('massdensity_matplotlib.png')
 
 figure=plt.figure(figsize=(4.5,4))
-plt.plot((mytable['RADRMS_EXTRAP_DECONV'][idx]), sigma, marker='s', linestyle='None')
+plt.plot(rgal, sigma, marker='s', linestyle='None')
 plt.xlabel(r'$R_{gal}\ (pc)$')
 plt.ylabel(r'${\sigma}_0$')
 plt.tight_layout()
