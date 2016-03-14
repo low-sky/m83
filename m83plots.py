237f3158c4989b4edb6ca1e12cd19e975c7ea3f0
Pamela <pafreema@ualberta.ca>
2016-03-13 23:30:15 -0700

    new catalog

--- m83plots.py
+++ m83plots.py
@@ -7,44 +7,64 @@
 import astropy
 import astropy.table
 import numpy as np
-#import some libraries
-from astropy.table import Table
+from astropy.table import Table, Column
 import matplotlib.pyplot as plt
+from galaxies import Galaxy
+import astropy.units as u
+
+mygalaxy=Galaxy("M83")
 
 #load the file
 mytable=Table.read('/home/pafreema/Documents/m83.co10.K_props_cprops.fits')
 #note that matplotlib understands LaTeX math in the code m
 
+rgal=mygalaxy.radius(ra=(mytable['XPOS']), dec=(mytable['YPOS']))
+colrgal=Column(name='RADIUS_PC',data=(rgal))
+mytable.add_column(colrgal)
+
 idx = np.where(np.isfinite(mytable['VIRMASS_EXTRAP_DECONV'])) #removes values that are infinite/NaN
+
 m,b=np.polyfit(np.log(mytable['MASS_EXTRAP'][idx]),np.log(mytable['VIRMASS_EXTRAP_DECONV'][idx]),1)
 figure=plt.figure(figsize=(4.5,4)) #figure size in inches
-plt.loglog(mytable['MASS_EXTRAP'],mytable['VIRMASS_EXTRAP_DECONV'],marker='s',linestyle='None')
-test_mass = np.logspace(5,8,100)
+plt.scatter(mytable['MASS_EXTRAP'],mytable['VIRMASS_EXTRAP_DECONV'], c=mytable['RADIUS_PC'], marker='s')
+test_mass = np.logspace(5,9,100)
 plt.plot(test_mass,np.exp(m*np.log(test_mass)+b)) #fit line for m83
 plt.plot(test_mass,test_mass,alpha=0.5,linewidth=2) #fitted x=y
 plt.xlabel(r'$M_{\mathrm{lum}}\ (M_{\odot})$')
 plt.ylabel(r'$M_{\mathrm{vir}}\ (M_{\odot})$')
+plt.axis([10e4, 10e7, 10e3, 10e8])
+plt.colorbar(label='$R_{gal}\ (pc)$')
+plt.xscale('log')
+plt.yscale('log')
 plt.tight_layout()
 plt.savefig('MlumMvir_matplotlib.png')
 
 n,c=np.polyfit(np.log(mytable['RADRMS_EXTRAP_DECONV'][idx]),np.log(mytable['VRMS_EXTRAP_DECONV'][idx]),1)
 figure=plt.figure(figsize=(4.5,4)) #figure size in inches
-plt.loglog(mytable['RADRMS_EXTRAP_DECONV'],mytable['VRMS_EXTRAP_DECONV'],marker='s',linestyle='None')
+plt.scatter(mytable['RADRMS_EXTRAP_DECONV'],mytable['VRMS_EXTRAP_DECONV'], c=mytable['RADIUS_PC'], marker='s')
 test_radius=np.logspace(0,3,100)
 plt.plot(test_radius, np.exp(n*np.log(test_radius)+c)) #fit line for m83
 plt.plot(test_radius, ((np.pi**0.5/3.4)**0.5)*(test_radius**0.5)) #fit line from Solomon et al.- using effective radius
 plt.xlabel(r'$R\ (pc)$')
 plt.ylabel(r'${\sigma}\  (km\ s^{-1})$')
+plt.axis([1,10e2, 10e-2, 10e1])
+plt.colorbar(label='$R_{gal}\ (pc)$')
+plt.xscale('log')
+plt.yscale('log')
 plt.tight_layout()
 plt.savefig('Rlinewidth_matplotlib.png')
 
 p,d=np.polyfit(np.log(mytable['RADRMS_EXTRAP_DECONV'][idx]),np.log(mytable['MASS_EXTRAP'][idx]),1)
 figure=plt.figure(figsize=(4.5,4)) #figure size in inches
-plt.loglog(mytable['RADRMS_EXTRAP_DECONV'],mytable['MASS_EXTRAP'],marker='s',linestyle='None')
+plt.scatter(mytable['RADRMS_EXTRAP_DECONV'],mytable['MASS_EXTRAP'],c=mytable['RADIUS_PC'], marker='s')
 test_mr=np.logspace(0,3,100)
 plt.plot(test_mr,np.exp(p*np.log(test_mr)+d)) #fit line for m83
 plt.plot(test_mr, (540*test_mr**(2))) #fit line from Solomon et al. - using effective radius
 plt.ylabel(r'$M_{\mathrm{lum}}\ (M_{\odot})$')
 plt.xlabel(r'$R\ (pc)$')
+plt.axis([1, 10e2, 10e4, 10e7])
+plt.colorbar(label='$R_{gal}\ (pc)$')
+plt.xscale('log')
+plt.yscale('log')
 plt.tight_layout()
 plt.savefig('MassRadius_matplotlib.png')
--- m83plots2.py
+++ m83plots2.py
@@ -17,17 +17,17 @@ mytable=Table.read('/home/pafreema/Documents/m83.co10.K_props_cprops.fits')
 idx = np.where(np.isfinite(mytable['VIRMASS_EXTRAP_DECONV'])) #removes values that are infinite/NaN
 
 rgal=mygalaxy.radius(ra=(mytable['XPOS'][idx]), dec=(mytable['YPOS'][idx]))
-print rgal #get radii in parsecs
+#print rgal #get radii in parsecs
 
 sigma=(mytable['VRMS_EXTRAP_DECONV'][idx])/np.sqrt((mytable['RADRMS_EXTRAP_DECONV'][idx])**2) #sigma naught
 dens=(mytable['MASS_EXTRAP'][idx])/(np.pi*((mytable['RADRMS_EXTRAP_DECONV'][idx])**2)) #mass density
 
-m,b=np.polyfit(dens,sigma,1)
-x=np.linspace(0,20000,100)
+m,b=np.polyfit(np.log(dens),np.log(sigma),1)
+x=np.linspace(10,10e3,100)
 figure=plt.figure(figsize=(4.5,4))
-plt.plot (dens, sigma, marker='s', linestyle='None')
-plt.plot(x, m*x+b)
-plt.xlabel(r'${\Sigma}(M)\ (M_{\odot}/pc^2)$')
+plt.loglog(dens, sigma, marker='s', linestyle='None')
+plt.loglog(x, np.exp(m*np.log(x)+b))
+plt.xlabel(r'${\Sigma}M\ (M_{\odot}/pc^2)$')
 plt.ylabel(r'${\sigma}_0\ $')
 plt.tight_layout()
 plt.savefig('massdensity_matplotlib.png')
