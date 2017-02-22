fceb596def127169cc7db5d9ba665936ce02117f
low_sky <erosolow@ualberta.ca>
2017-02-22 15:57:04 -0700

    changes for support ref report

--- m83plots.py
+++ m83plots.py
@@ -55,9 +55,9 @@ plt.scatter(mytable2['MASS_GCORR'],mytable2['VIRMASS_GCORR_DECONV'],
 test_mass = np.logspace(5,9,100)
 #plt.plot(test_mass,np.exp(m*np.log(test_mass)+b)) #fit line for m83
 plt.plot(test_mass,test_mass,alpha=0.5,linewidth=2) #fitted x=y
-plt.fill_between([1e5,3.35e5],1e5,5e7, color='gray',alpha=0.5)
+ax.fill_between([1e5,3.35e5],1e5,5e7, color='gray',alpha=0.5)
 virlim = 1040 * 2.57**2 * 20 *4 
-plt.fill_between([1e5,5e7],1e5,virlim, color='gray',alpha=0.5)
+ax.fill_between([1e5,5e7],1e5,virlim, color='gray',alpha=0.5)
 plt.text(0.05,0.95,'(a)',transform=ax.transAxes,va='top')
 plt.xlabel(r'$M_{\mathrm{lum}}\ (M_{\odot})$')
 plt.ylabel(r'$M_{\mathrm{vir}}\ (M_{\odot})$')
@@ -70,22 +70,22 @@ plt.tight_layout()
 
 #n,c=np.polyfit(np.log(mytable['RADRMS_GCORR_DECONV']),np.log(mytable['VRMS_GCORR_DECONV']),1)
 #figure=plt.figure(figsize=(4.5,4)) #figure size in inches
-ax = plt.subplot(222)
-plt.scatter(mytable['RADRMS_GCORR_DECONV'],mytable['VRMS_GCORR_DECONV'],
+ax2 = plt.subplot(222)
+ax2.scatter(mytable['RADRMS_GCORR_DECONV'],mytable['VRMS_GCORR_DECONV'],
             c=mytable['RADIUS_KPC'], marker='s',cmap='Greys',vmin=0,vmax=5)
-plt.scatter(mytable2['RADRMS_GCORR_DECONV'],mytable2['VRMS_GCORR_DECONV'],
+ax2.scatter(mytable2['RADRMS_GCORR_DECONV'],mytable2['VRMS_GCORR_DECONV'],
             c=mytable2['RADIUS_KPC'], marker='D',cmap='Greys',vmin=0,vmax=5,s=8)
 
 
 test_radius=np.logspace(0,3,100)
 #plt.plot(test_radius, np.exp(n*np.log(test_radius)+c)) #fit line for m83
-plt.plot(test_radius, ((np.pi**0.5/3.4)**0.5)*(test_radius**0.5)) #fit line from Solomon et al.- using effective radius
-plt.text(0.05,0.95,'(b)',transform=ax.transAxes,va='top')
-plt.xlabel(r'$R\ (\mathrm{pc})$')
-plt.ylabel(r'${\sigma_v}\  (\mathrm{km}\ \mathrm{s}^{-1})$')
-plt.axis([10,2e2, 1, 3*10e0])
-plt.fill_between([10,20],1e5,5e7,color='gray',alpha=0.5)
-plt.fill_between([10,200],1e5,3.3e5,color='gray',alpha=0.5)
+ax2.plot(test_radius, ((np.pi**0.5/3.4)**0.5)*(test_radius**0.5)) #fit line from Solomon et al.- using effective radius
+ax2.text(0.05,0.95,'(b)',transform=ax2.transAxes,va='top')
+ax2.set_xlabel(r'$R\ (\mathrm{pc})$')
+ax2.set_ylabel(r'${\sigma_v}\  (\mathrm{km}\ \mathrm{s}^{-1})$')
+ax2.axis([10,2e2, 1, 3*10e0])
+ax2.fill_between([10.,22.8],1e0,30,color='gray',alpha=0.5)
+ax2.fill_between([10.,200.],1e0, 2.57,color='gray',alpha=0.5)
 plt.colorbar(label='$R_{\mathrm{gal}}\ (\mathrm{kpc})$')
 plt.xscale('log')
 plt.yscale('log')
@@ -94,7 +94,7 @@ plt.tight_layout()
 
 #p,d=np.polyfit(np.log(mytable['RADRMS_GCORR_DECONV']),np.log(mytable['MASS_GCORR']),1)
 #figure=plt.figure(figsize=(4.5,4)) #figure size in inches
-ax = plt.subplot(223)
+ax3 = plt.subplot(223)
 plt.scatter(mytable['RADRMS_GCORR_DECONV'],mytable['MASS_GCORR'],
             c=mytable['RADIUS_KPC'], marker='s',cmap='Greys',vmin=0,vmax=5)
 plt.scatter(mytable2['RADRMS_GCORR_DECONV'],mytable2['MASS_GCORR'],
@@ -102,18 +102,18 @@ plt.scatter(mytable2['RADRMS_GCORR_DECONV'],mytable2['MASS_GCORR'],
 test_mr=np.logspace(0,3,100)
 #plt.plot(test_mr,np.exp(p*np.log(test_mr)+d)) #fit line for m83
 plt.plot(test_mr, (540*test_mr**(2))) #fit line from Solomon et al. - using effective radius
-plt.text(0.05,0.95,'(c)',transform=ax.transAxes,va='top')
+plt.text(0.05,0.95,'(c)',transform=ax3.transAxes,va='top')
 plt.ylabel(r'$M_{\mathrm{lum}}\ (M_{\odot})$')
 plt.xlabel(r'$R\ (\mathrm{pc})$')
 plt.axis([10, 2e2, 10e4, 5e7])
-plt.fill_between([10,20],1e0,30,color='gray',alpha=0.5)
-plt.fill_between([10,200],1e5,3.35,color='gray',alpha=0.5)
+ax3.fill_between([10,22.8],1e5,5e7,color='gray',alpha=0.5)
+ax3.fill_between([10,200],1e5,3.35e5,color='gray',alpha=0.5)
 #plt.colorbar(label='$R_{gal}\ (pc)$')
 plt.xscale('log')
 plt.yscale('log')
 plt.tight_layout()
 
-ax = plt.subplot(224)
+ax4 = plt.subplot(224)
 plt.scatter((mytable['MASS_GCORR'])/(mytable['RADRMS_GCORR_DECONV'])**2/np.pi,
             (mytable['VRMS_GCORR_DECONV']**2)/(mytable['RADRMS_GCORR_DECONV']),
             marker='s',cmap='Greys',c=mytable['RADIUS_KPC'],vmin=0,vmax=5)
@@ -123,7 +123,7 @@ plt.scatter((mytable2['MASS_GCORR'])/(mytable2['RADRMS_GCORR_DECONV'])**2/np.pi,
 test_mr=np.logspace(1,4,10)
 #plt.plot(test_mr,np.exp(p*np.log(test_mr)+d)) #fit line for m83
 plt.plot(test_mr, test_mr/300) #fit line from Solomon et al. - using effective radius
-plt.text(0.05,0.95,'(d)',transform=ax.transAxes,va='top')
+plt.text(0.05,0.95,'(d)',transform=ax4.transAxes,va='top')
 plt.ylabel(r'$\sigma_v^2/R\ (\mathrm{km^2\ s^2\ pc}^{-1})$')
 plt.xlabel(r'$\Sigma\ (M_{\odot}\ \mathrm{pc}^{-2})$')
 plt.colorbar(label='$R_{\mathrm{gal}}\ (\mathrm{kpc})$')
